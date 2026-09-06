a56c9c18ab94d2e856d6029d9c6136c772e67bb3
ANISH AGARWAL <agarwalanish270902@gmail.com>
2026-09-06 11:59:04 +0530

    Add solution for Employee Names (Employee Names)

---
+++ SQL/Employee Names/Employee Names.sql
@@ -0,0 +1,12 @@
+/*
+Problem Name: Employee Names
+Domain: SQL
+Subdomain: Employee Names
+URL: https://www.hackerrank.com/challenges/name-of-employees/problem
+Submitted Date: 2026-09-06
+Repository: Anni27-hub/Hackerrank_SQL
+*/
+
+SELECT NAME
+FROM Employee
+ORDER BY NAME;
