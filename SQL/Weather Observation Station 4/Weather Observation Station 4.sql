e8fe501ded5fe760a44cdfca60e5caba845ceebc
ANISH AGARWAL <agarwalanish270902@gmail.com>
2026-09-13 00:45:33 +0530

    Add solution for Weather Observation Station 4 (Weather Observation Station 4)

---
+++ SQL/Weather Observation Station 4/Weather Observation Station 4.sql
@@ -0,0 +1,11 @@
+/*
+Problem Name: Weather Observation Station 4
+Domain: SQL
+Subdomain: Weather Observation Station 4
+URL: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+Submitted Date: 2026-09-12
+Repository: Anni27-hub/Hackerrank_SQL
+*/
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
